45e2a3750bfadbf4a5bfdbf5518a223efdb132e4
jehala <jennifer.ehala@gmail.com>
2021-11-29 22:51:00 -0500

    commit

---
+++ CafeUtil.java
@@ -0,0 +1,12 @@
+public class TestCafe {
+
+    public String getStreakGoal(num) {
+        sum = num 
+        for(int i =0; i<10; i++){
+            sum += sum + [i]
+        }
+        return sum
+    }
+
+
+    
